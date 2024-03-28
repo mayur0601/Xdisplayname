2c2c41c7c1e6c1f3fad59bcc45fde8ddebd7be90
Mayur Shinde <24770409-MayurShinde9@users.noreply.replit.com>
2024-03-28 14:16:51 +0000

    change type ofinput to button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-import { useRef } from 'react';
+import { useState } from "react";
+import "./App.css";
+import { useRef } from "react";
 
 export default function App() {
+  let [displayName, setDisplayName] = useState(false);
+  let [fullname, setFullname] = useState("");
 
-  let [displayName,setDisplayName] = useState(false);
-  let [fullname,setFullname] = useState("");
-  
-  let  name = useRef("");
+  let name = useRef("");
   let lastName = useRef("");
 
   const handleFormSubmit = () => {
@@ -15,18 +14,22 @@ export default function App() {
     console.log(name.current.value);
     console.log(lastName.current.value);
     setFullname(`Full Name: ${name.current.value} ${lastName.current.value}`);
-  }
-  
-  
+  };
+
   return (
     <main>
-    <h1>Full Name Display</h1>
+      <h1>Full Name Display</h1>
       <label>First Name: </label>
-      <input type= "text" id="fname" name="fname" ref={name}/><br/>
+      <input type="text" id="fname" name="fname" ref={name} />
+      <br />
       <label>Last Name: </label>
-      <input type= "text" id="fname" name="fname" ref={lastName}/><br/>
-      <input type='submit' onClick={handleFormSubmit}></input><br/>
+      <input type="text" id="fname" name="fname" ref={lastName} />
+      <br />
+      <button type="submit" onClick={handleFormSubmit}>
+        Submit
+      </button>
+      <br />
       {displayName && <p>{fullname}</p>}
     </main>
-  )
+  );
 }
